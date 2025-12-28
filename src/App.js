bca9eea078278a2fe55ebbef40b834c369c22f17
Yi Cheng <ycheng5@andrew.cmu.edu>
2025-12-28 14:21:28 -0500

    new logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import myLogo from './assets/mylogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={myLogo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
