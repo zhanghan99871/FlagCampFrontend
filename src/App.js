42f3b2f485d1619f9e20c4b50aff917b7b8515a6
yl001-ma <yan668@gmail.com>
2026-01-09 20:48:25 -0500

    added new Route for iteneraryList page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
 
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import HelloProtected from './pages/HelloProtected';
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import HelloProtected from "./pages/HelloProtected";
+import ItineraryList from "./pages/ItineraryList";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Navigate to="/auth/login" replace />} />
-                <Route path="/auth/register" element={<Register />} />
-                <Route path="/auth/login" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/hello" element={<HelloProtected />} />
-                <Route path="*" element={<Navigate to="/auth/login" replace />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/auth/login" replace />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/hello" element={<HelloProtected />} />
+        <Route path="/itinerary/:itineraryId" element={<ItineraryList />} />
+        <Route path="*" element={<Navigate to="/auth/login" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
